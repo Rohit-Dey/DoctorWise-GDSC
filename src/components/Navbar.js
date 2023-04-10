92db45caf33a730e04137fe2267ba9068341c820
Vir <gulativir@gmail.com>
2023-04-10 19:14:28 +0530

    responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,70 +1,169 @@
-import React from 'react'
-import { NavLink, useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+import { Transition } from "@headlessui/react";
+import { NavLink } from "react-router-dom";
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    
-<nav className="bg-gray-900 text-white">
-  <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-    <div className="relative flex h-16 items-center justify-between">
-      <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-        
-        <button type="button" className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-          <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-          <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
-      </div>
-      <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <div className="flex flex-shrink-0 items-center space-x-3">
-          
-          <img className="block h-8 w-auto lg:hidden" src="/logo.png" alt="Your Company"/>
-          <img className="hidden h-8 w-auto lg:block" src="/logo.png" alt="Your Company"/>
-          <h2 className='text-2xl'>DoctorWise</h2>
-        </div>
-        <div className="hidden sm:ml-6 sm:block">
-          <div className="flex space-x-8 text-lg  md:px-24">
-            
-            
+    <div>
+      <nav className="bg-gray-900">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex space-x-2">
+                <img
+                  className="h-8 w-8"
+                  src="/logo.png"
+                  alt="Workflow"
+                />
+                <h2 className="text-2xl text-white">DoctorWise</h2>
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-24 flex items-baseline space-x-8">
+                  
 
-            <a href="#" className="text-white   px-3 py-2 text-lg   border-b-4 border-transparent hover:border-white">Home</a>
+                  <NavLink
+                    to="/"
+                    className="text-gray-300 border-b-4 border-transparent hover:border-white px-3 py-2  text-lg font-medium"
+                  >
+                    Home
+                  </NavLink>
 
-            <a href="#" className="text-white  px-3 py-2 text-lg   border-b-4 border-transparent hover:border-white">Find A Doctor</a>
+                  <NavLink
+                    to="/finddoctor"
+                    className="text-gray-300 border-b-4 border-transparent hover:border-white px-3 py-2  text-lg font-medium"
+                  >
+                    Find A Doctor
+                  </NavLink>
 
-            <a href="#" className="text-white   px-3 py-2 text-lg   border-b-4 border-transparent hover:border-white">Contact Us</a>
+                  <NavLink
+                    to="/contact"
+                    className="text-gray-300 border-b-4 border-transparent hover:border-white px-3 py-2  text-lg font-medium"
+                  >
+                    Contact Us
+                  </NavLink>
+                  
+                  <div className="log md:pl-64">
+
+                  <NavLink
+                    to="/login"
+                    className="text-gray-300 border-b-4 border-transparent hover:border-white px-3 py-2  text-lg font-medium md:mr-2"
+                  >
+                    Login
+                  </NavLink>
+                  
+                  <NavLink
+                    to="/register"
+                    className="text-gray-300 border-b-4 border-transparent hover:border-white px-3 py-2  text-lg font-medium "
+                  >
+                    Sign Up
+                  </NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="log">
-      <div className="flex space-x-8 text-lg  ">
-        <NavLink className=" border-b-4 border-transparent hover:border-white">Login</NavLink>
-        <NavLink className=" border-b-4 border-transparent hover:border-white">Sign Up</NavLink>
-        </div>
-      </div>
-    </div>
-  </div>
 
-  <div className="sm:hidden" id="mobile-menu">
-    <div className="space-y-1 px-2 pb-3 pt-2">
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            <div className="md:hidden" id="mobile-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <NavLink
+                  to="/"
+                  className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Home
+                </NavLink>
+
+                <NavLink
+                  to="/finddoctor"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Find A Doctor
+                </NavLink>
+
+                <NavLink
+                  to="/contact"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Contact Us
+                </NavLink>
+
+                <NavLink
+                  to="/login"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Login
+                </NavLink>
+
+                <NavLink
+                  to="/register"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Sign Up
+                </NavLink>
+              </div>
+            </div>
+          )}
+        </Transition>
+      </nav>
+
       
-      <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-
-      <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
-
-      <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
-
-      <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
+      
     </div>
-  </div>
-</nav>
-
-
-
-
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
