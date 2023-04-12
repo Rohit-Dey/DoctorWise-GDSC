5ee8e7e7a333913624ca3f714c4f980e19034d51
rohit-dey <rohitdey1432@gmail.com>
2023-04-12 13:10:08 +0530

    added user to db

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const router=require("./routes/router")
 const dbUrl ='mongodb://localhost:27017/doctor-wise'
 var bodyParser = require('body-parser')
+const User = require('./Models/User')
 
 
 
@@ -23,10 +24,11 @@ app.use(bodyParser.urlencoded())
 
 app.use(bodyParser.json())
 
-app.post("/register", function (req, res) {
-    console.log(req.body) // populated!
-    res.send(200, req.body);
-  });
+app.post("/register", async function (req, res) {
+    const newUser = new User(req.body);
+    await newUser.save();
+    console.log(newUser);
+});
 
 app.listen(3000, () => {
     console.log('Serving On Port 4000')
