404b36e3ddea3ea8cb123b73b00de62cb854ec43
Vir <gulativir@gmail.com>
2023-04-13 19:16:13 +0530

    login and register routes perfected with login now creating tokens and cookies

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
+require("dotenv").config()
 const mongoose = require('mongoose');
 const router=require("./routes/router")
 const dbUrl = 'mongodb+srv://rohitdey:vAj7wdRuC1j1iQ4H@cluster0.mvwhven.mongodb.net/?retryWrites=true&w=majority'
 var bodyParser = require('body-parser')
 const User = require('./models/User')
 const cors=require("cors")
-
-
+const bcrypt = require("bcryptjs");
+const cookieParser = require("cookie-parser");
 
 mongoose.connect(dbUrl)
 
@@ -18,12 +19,54 @@ db.once("open", () => {
 
 const app = express();
 app.use(express.json())
-app.use(express.urlencoded({ extended: false })) //to view req.body otherwise it is empty by default
+app.use(express.urlencoded({ extended: true })) //to view req.body otherwise it is empty by default
+app.use(cookieParser(""));
 app.use(cors())
 //app.use(bodyParser.urlencoded())
 //app.use(router)
 
 //app.use(bodyParser.json())
+app.post("/login", async (req, res) => {
+    // console.log(req.body);
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        res.status(400).json({ error: "fill the details" });
+    };
+
+    try {
+
+        const userlogin = await User.findOne({ email: email });
+        console.log(userlogin);
+        if (userlogin) {
+            const isMatch = await bcrypt.compare(password, userlogin.password);
+            console.log(isMatch);
+
+
+
+            if (!isMatch) {
+                res.status(400).json({ error: "invalid crediential pass" });
+            } else {
+                
+                const token = await userlogin.generatAuthtoken();
+                console.log(token);
+
+                res.cookie("doctorwise", token, {
+                    expires: new Date(Date.now() + 2589000),
+                    httpOnly: true
+                });
+               res.status(201).json(userlogin);
+            }
+
+        } else {
+            res.status(400).json({ error: "user not exist" });
+        }
+
+    } catch (error) {
+        res.status(400).json({ error: "invalid crediential pass" });
+        console.log("error the bhai catch ma for login time" + error.message);
+    }
+});
 
 app.post("/register", async function (req, res) {
     const { name, email, password} = req.body;
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const bcrypt = require("bcryptjs");
+const keysecret = process.env.KEY
+const jwt = require("jsonwebtoken");
 const userSchema = new Schema({
     name:{
         type: String,
@@ -14,9 +16,35 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true
-    }
+    },tokens:[
+        {
+            token:{
+                type:String,
+                required:true
+            }
+        }
+    ]
+    
 })
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
+        this.password = await bcrypt.hash(this.password, 12);
+        
+    }
+    next();
+});
+userSchema.methods.generatAuthtoken = async function(){
+    try {
+        let token = jwt.sign({ _id:this._id},keysecret,{
+            expiresIn:"1d"
+        });
+        this.tokens = this.tokens.concat({token:token});
+        await this.save();
+        return token;
 
-//userSchema.plugin(passportLocalMongoose)
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 module.exports = mongoose.model('User', userSchema);
