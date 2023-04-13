087ed589cdaae9e25251f6d9221fcdeed962db4f
rohit-dey <rohitdey1432@gmail.com>
2023-04-14 00:05:14 +0530

    search

--- src/components/AutoCompleteSearchBar.js
+++ src/components/AutoCompleteSearchBar.js
@@ -40,13 +40,12 @@ export default function Example() {
         );
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="fixed top-16 w-72">
+   
         <Combobox value={selected} onChange={setSelected}>
-          <div className="relative mt-1">
+          <div className="relative mt-14">
             <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
               <Combobox.Input
-                className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+                className="w-full border-none py-4 pl-3 pr-10 text-lg leading-5 text-gray-900 focus:ring-0"
                 displayValue={(person) => person.name}
                 onChange={(event) => setQuery(event.target.value)}
               />
@@ -64,7 +63,7 @@ export default function Example() {
               leaveTo="opacity-0"
               afterLeave={() => setQuery("")}
             >
-              <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <Combobox.Options className="relative mt-1 max-h-80 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
                 {filteredPeople.length === 0 && query !== "" ? (
                   <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
                     Nothing found.
@@ -110,7 +109,6 @@ export default function Example() {
             </Transition>
           </div>
         </Combobox>
-      </div>
-    </div>
+
   );
 }
